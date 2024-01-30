5b462a53f3ef00fd9e47e8e780a4ffe554b27068
Error_Account <66193775+sarojlol@users.noreply.github.com>
2024-01-30 21:42:10 +0700

    modified:   src/main.cpp 	modified:   src/pin_define.h

--- src/main.cpp
+++ src/main.cpp
@@ -13,14 +13,19 @@ int last_distance;
 
 
 void setup() {
+  //สั่งเริ่มทำงานจอ LCD
   lcd.init();
   lcd.backlight();
+
+  //สั่งหน้าจอ Splash Screen
   lcd.clear();
   lcd.setCursor(4,0);
   lcd.print("Wireless");
   lcd.setCursor(2, 1);
   lcd.print("Tape Measure");
   delay(2000);
+
+  //สั่งพิมตัวอักศรคงที่ลงจอ LCD
   lcd.clear();
   lcd.setCursor(4, 0);
   lcd.print("0-400 CM");
@@ -30,31 +35,38 @@ void setup() {
 
 void loop()
 {
+  //อ่านค่าทุกๆ 200mS
   static unsigned long distance_fillter;
   if ((millis() - distance_fillter) > 200)
   {
     distance = sonar.ping_cm();
     distance_fillter = millis();
   }
+
+  //ถ้าระยะทางที่อ่านไม่เท่าเดิมถึงจะupdateค่าลงจอLCD
   if (distance != last_distance){
     if (distance < 380)
     {
       lcd.setCursor(10, 1);
+      //ถ้าค่าตำกว่า 99 ให้ใส่ช่องว่างต่อท้ายช่องเดียว
       if (distance > 99)
       {
         lcd.print(String(distance) + " ");
       }
+      //ถ้าค่าน้อยกว่าหรือเท่ากับให้ใส่ช่องว่างต่อท้าง 2 ช่อง
       else if (distance <= 99)
       {
         lcd.print(String(distance) + "  ");
       }
+      //แสดงคำว่า CM ต่อท้าย
       lcd.setCursor(14, 1);
       lcd.print("CM");
     }
+    //ถ้าค่าเกิน380ให้แสดงคำว่า OL (Over Range)
     else if (distance >=380){
       lcd.setCursor(10, 1);
       lcd.print("OL     ");
     }
-    last_distance = distance;
+    last_distance = distance; //กำหนดให้ค่าที่แล้วเท่ากับค่าปัจจุบัญ
   }
 }
--- src/pin_define.h
+++ src/pin_define.h
@@ -1,3 +1,3 @@
-#define TRIGGER_PIN  3  // Arduino pin tied to trigger pin on the ultrasonic sensor.
-#define ECHO_PIN     2  // Arduino pin tied to echo pin on the ultrasonic sensor.
-#define MAX_DISTANCE 400
+#define TRIGGER_PIN  3  //ขา Trig ของเซนเซอร์
+#define ECHO_PIN     2  //ขา Echo ของเซนเซอร์
+#define MAX_DISTANCE 400 //ระยะทางสูงสุดที่วัดได้
